3c1e224d6d7f4a976ff4552d052e14352174c5ee
MaxE688 <max.evers8@gmail.com>
2024-08-30 16:57:50 -0400

    Styling template

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,7 @@
 (function($) {
 
 	var	$window = $(window),
+		$document = $(document),
 		$body = $('body'),
 		$nav = $('#nav');
 
@@ -23,6 +24,84 @@
 			window.setTimeout(function() {
 				$body.removeClass('is-preload');
 			}, 100);
+
+			projects.forEach(function(proj, index) {
+	
+				// Create card, append to DOM
+				let $card = $("<div class='col-4 col-6-medium col-12-small card'></div>");
+				let $cardArticle = $("<article class='box style2'></article>");
+				$card.append($cardArticle);
+				$cardArticle.append($("<div class='image featured'><img src='"+ proj.img +"' alt='" + proj.alt + "' /></div>"));
+				$cardArticle.append($("<h3>" + proj.title + "</h3>"));
+				$cardArticle.append($("<p>" + proj.shortDesc + "</p>"));
+				$("#card-container").append($card);
+	
+				// Create modal, append to DOM
+				let $modal = $("<div class='modal'></div>");
+				let $modalContent = $("<div class='modal-conent'></div>");
+				let $closeModal = $("<div class='close-modal'></div>");
+				let $content = $("<div class='container flex' ></div>");
+	
+				// create main content element for modal
+				$content.append($("<h1 class='modal-title'> "+ proj.title +"</>"));
+				$content.append($("<hr class='modal-title-rule'/>"))
+				$content.append($("<img src='"+ proj.img +"'/>"));
+				$content.append($("<p>"+ proj.longDesc +"</p>"));
+	
+				// create close button for modal
+				$closeModal.append($("<img class='close-modal-img' id='close-modal-1' src='./images/SVG/close-circle-dark.svg' />"));
+				$closeModal.append($("<img class='close-modal-img' id='close-modal-2' src='./images/SVG/close-circle-light.svg' />"));
+				
+				// append elements to finish creation of modal
+				$modalContent.append($closeModal);
+				$modalContent.append($content);
+				$modal.append($modalContent);
+	
+				$("#modal-container").append($modal);
+			});
+
+			
+		});
+		
+		$document.ready(function(){
+
+			$(".modal").each(function(index){
+				var id = "modal-" + index;
+				var closeID = "#" + id;
+				$(this).attr("id", id);
+ 
+				$(this).find(".close-modal").click(function(){
+					console.log("close me:",id);
+					$(closeID).removeClass("in");
+				});
+			});
+
+			$(".box").each(function(index){
+				var id = "box-" + index;
+				$(this).attr("id", id);
+
+				$(this).click(function(){
+					console.log("open me");
+					$("#modal-"+index).addClass("in");
+				});
+
+			});
+
+
+
+			// Closes the project preview
+			// $(".close-modal").click(function(){
+			// 	console.log("Closed");
+			// 	$(".modal").toggleClass("in");
+			// });
+			
+			// Opens the project preview
+			// $(".box").click(function(){
+			// 	console.log("Opened");
+			// 	$(".modal").toggleClass("in");
+			// });
+
+
 		});
 
 	// Scrolly.
---
+++ assets/js/project-data.js
@@ -0,0 +1,29 @@
+const projects = [
+  {
+    title: "Project 1",
+    img: "images/pic01.jpg",
+    alt: "Project 1 img",
+    shortDesc: "Ornare nulla proin odio consequat.",
+    longDesc: "Ornare nulla proin odio consequat. Ornare nulla proin odio consequat.",
+    demo: "",
+    source: ""
+  },
+  {
+    title: "Project 2",
+    img: "images/pic02.jpg",
+    alt: "Project 2 img",
+    shortDesc: "Ornare nulla proin odio consequat.",
+    longDesc: "Ornare nulla proin odio consequat. Ornare nulla proin odio consequat.",
+    demo: "",
+    source: ""
+  },
+  {
+    title: "Project 3",
+    img: "images/pic03.jpg",
+    alt: "Project 2 img",
+    shortDesc: "Ornare nulla proin odio consequat.",
+    longDesc: "Ornare nulla proin odio consequat. Ornare nulla proin odio consequat.",
+    demo: "",
+    source: ""
+  },
+];
